82fa0062a09e253feed3904876b9c81cc5980d43
Blackbaud-ToddRoberts <todd.roberts@blackbaud.com>
2017-08-09 14:23:12 -0400

    alert page

---
+++ src/app/components/shared/demo-page-accessibility.component.ts
@@ -0,0 +1,11 @@
+import {
+  ChangeDetectionStrategy,
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-demo-page-accessibility',
+  templateUrl: './demo-page-accessibility.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SkyDemoAccessibilityComponent { }
---
+++ src/app/components/shared/demo-page-guidelines.component.ts
@@ -0,0 +1,11 @@
+import {
+  ChangeDetectionStrategy,
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-demo-page-guidelines',
+  templateUrl: './demo-page-guidelines.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SkyDemoGuidelinesComponent { }
